43da3c91a10ce3888939ce640f8a83f273ce8c0e
Taylor Newman <newmant@uoguelph.ca>
2022-01-08 15:21:58 -0500

    Combined with backend

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict'
+const express = require('express')
+require('dotenv').config()
+const app = express();
+const path = require("path");
+
+
+app.use(express.static(path.join(__dirname, process.env.REACT_LOCATION)));
+const portNum = process.env.PORT | 8001;
+
+const postList = [
+    {
+        title: "My First Blog Post",
+        href: "my-first-blog-post"
+    }
+]
+
+const postLocation = [
+    {
+        "my-first-blog-post": path.join(__dirname, 'blog_posts', 'a.md')
+    }
+]
+
+app.get('/api/blog-post-list', function (req, res) {
+    res.json(postList);
+});
+
+app.get('/api/posts/*', function (req, res) {
+    let urlArray = req.url.split("/");
+    let url = urlArray[urlArray.length - 1];
+    for(let item in postList){
+        if (postList[item]['href'] === url)
+        {
+            return res.sendFile(path.join(__dirname, 'blog_posts', url + '.md'));
+        }
+    }
+    res.status(404).send('# Sorry, Couldn\'t find that post');
+});
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, process.env.REACT_LOCATION, '/index.html'));
+});
+
+app.listen(portNum, () => {
+    console.log(`Running on http://localhost:${portNum}`)
+});
+
