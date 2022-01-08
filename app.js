10de880d026d82a84616d5bd4212ac86f2e9967e
Taylor Newman <newmant@uoguelph.ca>
2022-01-08 15:46:53 -0500

    changed more heroku settings, bad port setter

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 
 app.use(express.static(path.join(__dirname, process.env.REACT_LOCATION)));
-const portNum = process.env.PORT | 8001;
+const portNum = process.env.PORT || 8001;
 
 const postList = [
     {
